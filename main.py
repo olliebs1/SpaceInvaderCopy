fe49eecbcb47704e4b6bd5528bb7c7a4963ed2da
Olivier benson-smith <olliebenson@hotmail.co.uk>
2020-04-24 21:05:12 +0100

    laser class create

--- main.py
+++ main.py
@@ -42,6 +42,26 @@ BG = pygame.transform.scale(pygame.image.load(
     os.path.join('assets', 'background-black.png')), (Width, Height))
 
 
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+        def draw(self, window):
+            window.blit(self.img, (self.x, self.y))
+
+        def move(self, vel):
+            self.y += vel
+
+        def off_screen(self, height):
+            return self.y < height and self.y >= 0
+
+        def collision(self, obj):
+            return collide(obj, self)
+
+
 class Ship:
     def __init__(self, x, y, health=100):
         self.x = x
