40fa74eb2423d1c35d18c3b8728bd899ae0f039a
Olivier benson-smith <olliebenson@hotmail.co.uk>
2020-04-24 16:54:55 +0100

    main.py created and images and backgrounds imported along with window page being set

---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame
+import os
+import time
+import random
+
+
+Width, Height = 750, 750
+WIN = pygame.display.set_mode((Width, Height))
+pygame.display.set_caption('Space Invaders Copy')
+
+# Ships
+redSpaceShip = pygame.image.load(
+    os.path.join('assets', 'pixel_ship_red_small.png'))
+
+greenSpaceShip = pygame.image.load(
+    os.path.join('assets', 'pixel_ship_green_small.png'))
+
+blueSpaceShip = pygame.image.load(
+    os.path.join('assets', 'pixel_ship_blue_small.png'))
+
+# Player Ship
+yellowSpaceShip = pygame.image.load(
+    os.path.join('assets', 'pixel_ship_yellow.png'))
+
+# Lasers
+redLaser = pygame.image.load(
+    os.path.join('assets', 'pixel_laser_red.png'))
+
+greenLaser = pygame.image.load(
+    os.path.join('assets', 'pixel_laser_green.png'))
+
+blueLaser = pygame.image.load(
+    os.path.join('assets', 'pixel_laser_blue.png'))
+
+yellowLaser = pygame.image.load(
+    os.path.join('assets', 'pixel_laser_yellow.png'))
+
+# Background
+
+BG = pygame.image.load(
+    os.path.join('assets', 'background-black.png'))
